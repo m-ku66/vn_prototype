5a5f2cb1a3b1087a9f08b090159e4ee87234865a
m-ku66 <johanmarcmiango15@gmail.com>
2025-08-04 12:09:29 -0400

    tailwindcss and typescript comfiguration

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,4 +1,16 @@
+// import { defineConfig } from 'vite';
+
+// // https://vitejs.dev/config
+// export default defineConfig({});
+
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  plugins: [
+    react(), // Since we have React installed
+    tailwindcss(), // TailwindCSS v4 Vite plugin
+  ],
+});
